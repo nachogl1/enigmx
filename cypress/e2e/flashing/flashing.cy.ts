546bc41f8ed6eee00ea943e469df14f50809df50
nachogl1 <i.gonzalezl@hotmail.com>
2024-08-29 17:13:51 +0100

    test: cypress ready && git push

--- cypress/e2e/flashing/flashing.cy.ts
+++ cypress/e2e/flashing/flashing.cy.ts
@@ -1,6 +1,8 @@
 describe('Flashing', () => {
   it('screen should be open when tapping on icon', () => {
     cy.visit('/');
-    cy.get('.button').click();
+    cy.get('[data-testid="sideMenu__button"]').click();
+    cy.get('[data-testid="sideMenu__button__flash"]').click();
+    cy.contains("FLASHING");
   })
 })
