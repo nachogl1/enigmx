448ffeb031fd0156bdc7dc93a016123eeaf04936
nachogl1 <i.gonzalezl@hotmail.com>
2024-08-29 19:36:32 +0100

    wip: reading modal dialog

--- src/pages/reading/ReadingPage.tsx
+++ src/pages/reading/ReadingPage.tsx
@@ -1,11 +1,30 @@
-import { IonPage } from '@ionic/react';
-import React from 'react';
+import { IonButton, IonButtons, IonContent, IonHeader, IonModal, IonSpinner, IonTitle, IonToolbar } from '@ionic/react';
+import React, { useState } from 'react';
 
 const ReadingPage: React.FC = () => {
+  const [isReading, setReading] = useState(false);
+
   return (
-    <IonPage>
-      READING
-    </IonPage>
+    <div style={{ height: "100%", alignContent: "center", textAlign: "center" }}>
+
+      {!isReading && <IonButton onClick={() => setReading(true)} fill="outline">START READING</IonButton>}
+
+      <IonModal data-testid="reading__loading-icon" isOpen={isReading}>
+        <IonHeader>
+          <IonToolbar>
+            <IonButtons slot="end">
+              <IonButton onClick={() => setReading(false)}>Close</IonButton>
+            </IonButtons>
+          </IonToolbar>
+        </IonHeader>
+        <IonContent className="ion-padding">
+          <p>
+            Loading Icon for Reading here
+          </p>
+        </IonContent>
+      </IonModal>
+
+    </div>
   );
 };
 
--- src/pages/reading/__tests__/readingPage.test.tsx
+++ src/pages/reading/__tests__/readingPage.test.tsx
@@ -1,9 +1,26 @@
-import { render } from "@testing-library/react";
+import { fireEvent, render, waitFor } from "@testing-library/react";
 import ReadingPage from "../ReadingPage";
 
 describe("Reading Page should", () => {
-    it('render correctly', () => {
+
+    it('renders READ button', () => {
+        const { getByText } = render(<ReadingPage></ReadingPage>);
+        expect(getByText("START READING")).toBeInTheDocument();
+    });
+
+    it('start read when clicking START READING', () => {
+        const { getByText, getByTestId } = render(<ReadingPage></ReadingPage>);
+        const readButton = getByText("START READING");
         
+
+        waitFor(() => {
+            fireEvent.click(readButton);
+        });
+        
+        const loadingIcon = getByTestId("reading__loading-icon");
+        expect(readButton).not.toBeInTheDocument();
+        expect(loadingIcon).toBeInTheDocument();
+
     });
 
 })
