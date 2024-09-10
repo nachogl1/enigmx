986b216df7f4684187b52e57986a11c61b5b2c15
nachogl1 <i.gonzalezl@hotmail.com>
2024-09-10 22:26:30 +0100

    test: stable reading service

--- src/services/readingV2/__test__/readingV2.test.ts
+++ src/services/readingV2/__test__/readingV2.test.ts
@@ -1,41 +1,77 @@
-import { NfcTag } from '@awesome-cordova-plugins/nfc';
-import { Observable, of, throwError } from 'rxjs';
-import { describe, expect, it, vi } from 'vitest';
+import { NfcTag } from "@awesome-cordova-plugins/nfc";
+import { Observable, of, throwError } from "rxjs";
+import { describe, expect, it, vi } from "vitest";
 import { readFromNtagV2 } from "../readingV2.service";
 
-const nfcTag: NfcTag = {
-    type: "testTag",
+const payloadAsText = "U2FsdGVkX1891+OcRh6TL7GEetS4f2DGAu6UxEYX6es=";
+
+let nfcTag: NfcTag = {
+  id: [4, 53, -82, 42, 90, 102, -127],
+  techTypes: [
+    "android.nfc.tech.NfcA",
+    "android.nfc.tech.MifareUltralight",
+    "android.nfc.tech.Ndef",
+  ],
+  type: "NFC Forum Type 2",
+  maxSize: 868,
+  isWritable: true,
+  ndefMessage: [
+    {
+      tnf: 1,
+      type: [84],
+      id: [],
+      payload: [
+        2, 101, 110, 85, 50, 70, 115, 100, 71, 86, 107, 88, 49, 56, 57, 49, 43,
+        79, 99, 82, 104, 54, 84, 76, 55, 71, 69, 101, 116, 83, 52, 102, 50, 68,
+        71, 65, 117, 54, 85, 120, 69, 89, 88, 54, 101, 115, 61,
+      ],
+    },
+  ],
+  canMakeReadOnly: true,
 };
 
 let readerModeMock: Observable<NfcTag | Error> = of(nfcTag);
 
-vi.mock('@awesome-cordova-plugins/nfc', () => {
-    return {
-        NFC: {
-            readerMode: (flag: number) => readerModeMock,
-            FLAG_READER_NFC_A: 1,
-        }
+vi.mock("@awesome-cordova-plugins/nfc", () => {
+  return {
+    NFC: {
+      readerMode: (flag: number) => readerModeMock,
+      FLAG_READER_NFC_A: 1,
+    },
+  };
+});
+
+describe.only("Reading service should", () => {
+  beforeEach(() => {
+    vi.clearAllMocks();
+  });
+
+  it("return the read value", async () => {
+    const result = await readFromNtagV2();
+    expect(result).toBe(payloadAsText);
+  });
+
+  it("fail if read from ntag fails", async () => {
+    readerModeMock = throwError(() => "NFC Reading failed");
+
+    await expect(async () => await readFromNtagV2()).rejects.toThrowError(
+      /^NFC Reading failed$/
+    );
+  });
+
+  it.each([[], undefined])(
+    "fail if payload is not valid",
+    async (ndeMessageValue) => {
+      readerModeMock = of(nfcTag);
+
+      nfcTag = {
+        ...nfcTag,
+        ndefMessage: ndeMessageValue,
+      };
+
+      await expect(async () => await readFromNtagV2()).rejects.toThrowError(
+        /^Encrypted payload was not valid$/
+      );
     }
+  );
 });
-
-describe('Reading service should', () => {
-
-    beforeEach(() => {
-        vi.clearAllMocks();
-    });
-
-    it('return the read value', async () => {
-        const result = await readFromNtagV2();
-        expect(result?.type).toBe("testTag");
-    });
-
-    it('errors out gracefully', async () => {
-        readerModeMock = throwError(() => "Test Error Message")
-
-        await expect(async () =>
-            await readFromNtagV2()).
-            rejects.toThrowError(/^Test Error Message$/);
-    });
-
-});
-
--- src/services/readingV2/readingV2.service.ts
+++ src/services/readingV2/readingV2.service.ts
@@ -1,23 +1,48 @@
+import { NFC, NfcTag } from "@awesome-cordova-plugins/nfc";
+import { firstValueFrom } from "rxjs";
 
-import { NFC } from '@awesome-cordova-plugins/nfc';
-import { firstValueFrom } from 'rxjs';
-
+const startOfRealEncryptedMessage = "U2Fsd";
 
 export const readFromNtagV2: () => Promise<string> = async () => {
-    const payload = (await firstValueFrom(NFC.readerMode(getPredefinedFlags()))).ndefMessage?.pop()?.payload;
+  const ntagObject = await readFromNtag();
+  const payload = extractPayloadFrom(ntagObject);
+  const payloadAsText = bytesToText(payload);
+  const decodeMessageWithoutLanguagePrefix =
+    removeLanguagePrefix(payloadAsText);
+  return decodeMessageWithoutLanguagePrefix;
+};
 
-    //validation payload
-    const uint8bytes = new Uint8Array(payload!);
-    const decoded = new TextDecoder().decode(uint8bytes);
-    const firstIndex = decoded.indexOf("U2Fsd");
-    const bar = decoded.substring(firstIndex);
-
-    return bar;
+const readFromNtag = async () => {
+  return firstValueFrom(NFC.readerMode(getPredefinedFlags()));
 };
 
 const getPredefinedFlags = () => {
-    return NFC.FLAG_READER_NFC_A | NFC.FLAG_READER_NFC_V |
-        NFC.FLAG_READER_NFC_B | NFC.FLAG_READER_NFC_F;
-}
+  return (
+    NFC.FLAG_READER_NFC_A |
+    NFC.FLAG_READER_NFC_V |
+    NFC.FLAG_READER_NFC_B |
+    NFC.FLAG_READER_NFC_F
+  );
+};
 
+const extractPayloadFrom = (ntag: NfcTag): number[] => {
+  const payload = ntag.ndefMessage?.pop()?.payload;
 
+  if (!payload || payload?.length < 1) {
+    throw new Error("Encrypted payload was not valid");
+  }
+
+  return payload;
+};
+
+const bytesToText = (payload: number[]): string => {
+  const uint8bytes = new Uint8Array(payload);
+  const decoded = new TextDecoder().decode(uint8bytes);
+  return decoded;
+};
+
+const removeLanguagePrefix = (payloadAsText: string) => {
+  const firstIndexOfRealMessage = payloadAsText.indexOf(startOfRealEncryptedMessage);
+  const result = payloadAsText.substring(firstIndexOfRealMessage);
+  return result;
+};
