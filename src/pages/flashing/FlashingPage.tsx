2aa999e4640b694213f85706ae8fae897c114c6e
nachogl1 <i.gonzalezl@hotmail.com>
2024-09-10 16:49:40 +0100

    feature: including encryption for flahsing

--- src/pages/flashing/FlashingPage.tsx
+++ src/pages/flashing/FlashingPage.tsx
@@ -1,21 +1,24 @@
 import { IonButton, IonSpinner } from '@ionic/react';
 import React, { useState } from 'react';
+import { encryptMessage } from '../../services/encryption/encryption';
 import { flashNtag } from '../../services/flashing/flashing.service';
 
 const FlashingPage: React.FC = () => {
 
   const [message, setMessage] = useState<string>("");
-  const [password, setPassword] = useState<string>("");
+  const [pk, setPk] = useState<string>("");
   const [loading, setLoading] = useState<boolean>();
-  const [error, setError] = useState<string>("");
+  const [errorMessage, setErrorMessage] = useState<string>("");
 
   const flashHandler = () => {
-    setError("")
+    setErrorMessage("")
     setLoading(true);
 
-    flashNtag(message)
+    const encryptedMessage = JSON.stringify(encryptMessage(message, pk));
+
+    flashNtag(encryptedMessage)
       .catch((error) => {
-        setError(error);
+        setErrorMessage(error);
       })
       .finally(() => {
         setLoading(false);
@@ -37,18 +40,18 @@ const FlashingPage: React.FC = () => {
         <div className="input-group mb-3"
           style={{ width: "80%", marginBottom: "5vh" }}>
           <input data-testid="pk-input" type="password" className="form-control" placeholder="Private Key" onChange={(e) => {
-            setPassword(e.target.value as string)
+            setPk(e.target.value as string)
           }
           }></input>
         </div>
 
 
-        {(message && password) && <IonButton onClick={() => { flashHandler(); }} fill="outline">FLASH</IonButton>}
-        {(!message || !password) && <IonButton disabled fill="outline">FLASH</IonButton>}
+        {(message && pk) && <IonButton onClick={() => { flashHandler(); }} fill="outline">FLASH</IonButton>}
+        {(!message || !pk) && <IonButton disabled fill="outline">FLASH</IonButton>}
 
-        {error &&
+        {errorMessage &&
           <div className="alert alert-danger mt-2" data-testid="flashing__error" role="alert">
-            {error}
+            {errorMessage}
           </div>}
 
       </>}
--- src/pages/flashing/__tests__/flashingPage.test.tsx
+++ src/pages/flashing/__tests__/flashingPage.test.tsx
@@ -3,10 +3,17 @@ import { act } from "react";
 import { describe, expect, it, vi } from 'vitest';
 import FlashingPage from "../FlashingPage";
 
-
+const cipherParamsObject = {
+    message: "testMessageCyphered",
+};
 const flashNtagMock = vi.fn();
 vi.mock('../../../services/flashing/flashing.service', () => ({
     flashNtag: (message: string) => flashNtagMock(message),
+}))
+
+const encryptmessage = vi.fn();
+vi.mock('../../../services/encryption/encryption', () => ({
+    encryptMessage: (encryptPayload: string, privateKey: string) => encryptmessage(),
 }))
 
 describe("Flashing Page should", () => {
@@ -18,6 +25,8 @@ describe("Flashing Page should", () => {
 
     it('flash ntag', async () => {
         flashNtagMock.mockResolvedValue("");
+
+        encryptmessage.mockReturnValue(cipherParamsObject);
 
         const { getByText, getByTestId, queryByTestId } = render(<FlashingPage></FlashingPage>);
         expect(getByTestId("message-input")).toBeInTheDocument();
@@ -42,7 +51,7 @@ describe("Flashing Page should", () => {
 
         await waitFor(
             () => {
-                expect(flashNtagMock).toHaveBeenCalledWith("testMessage");
+                expect(flashNtagMock).toHaveBeenCalledWith(JSON.stringify(cipherParamsObject));
                 expect(queryByTestId("flashing__error")).not.toBeInTheDocument();
 
             }
